ccc0c5781a1e75eac623596b02f47cf43130bf17
Alexandr Makarov <designer.404@yandex.ru>
2022-01-29 15:34:42 +0300

    create scripts

---
+++ Assets/PixelCrew/Hero.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private float _direction;
+
+    public void SetDirection(float direction)
+    {
+        _direction = direction;
+    }
+
+    private void Update()
+    {
+
+    }
+}

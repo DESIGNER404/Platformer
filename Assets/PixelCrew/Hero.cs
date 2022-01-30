6ed6e5280f7f2599583a12bb9fa8a02e196eb0ab
Alexandr Makarov <designer.404@yandex.ru>
2022-01-30 11:59:44 +0300

    right walk

--- Assets/PixelCrew/Hero.cs
+++ Assets/PixelCrew/Hero.cs
@@ -15,6 +15,11 @@ public class Hero : MonoBehaviour
 
     private void Update()
     {
-
+        if (_direction != 0)
+        {
+            var delta = _direction * _speed * Time.deltaTime;
+            var newXPosition = transform.position.x + delta;
+            transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
+        }
     }
 }
--- Assets/PixelCrew/HeroInputReader.cs
+++ Assets/PixelCrew/HeroInputReader.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class HeroInputReader : MonoBehaviour
 {
-    private Hero _hero;
+    [SerializeField] private Hero _hero;
 
     private void Awake()
     {

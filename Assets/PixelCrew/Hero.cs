f77ea5cf2105a982d729ae8ef99bcfa4e5001c50
Alexandr Makarov <designer.404@yandex.ru>
2022-08-10 19:50:39 +0300

    add animations

--- Assets/PixelCrew/Hero.cs
+++ Assets/PixelCrew/Hero.cs
@@ -10,6 +10,12 @@ namespace PixelCrew
         [SerializeField] private LayerCheck _groundCheck;
 
         private Rigidbody2D _rigidbody;
+        private Animator _animator;
+        private SpriteRenderer _sprite;
+
+        private static readonly int IsGroundKey = Animator.StringToHash("is-ground");
+        private static readonly int VerticalVelocity = Animator.StringToHash("vertical-velocity");
+        private static readonly int IsRunning = Animator.StringToHash("is-running");
 
         private float _directionHorizontal, _directionVertical;
 
@@ -17,6 +23,8 @@ namespace PixelCrew
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
+            _sprite = GetComponent<SpriteRenderer>();
         }
 
         public void SetDirectionHorizontal(float direction)
@@ -35,9 +43,10 @@ namespace PixelCrew
             _rigidbody.velocity = new Vector2(_directionHorizontal * _speed, _rigidbody.velocity.y);
 
             var isJumping = _directionVertical > 0;
+            var isGrounded = IsGrounded();
             if (isJumping)
             {
-                if (IsGrounded())
+                if (IsGrounded() && _rigidbody.velocity.y <= 0)
                 {
                     _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
                 }
@@ -48,6 +57,24 @@ namespace PixelCrew
             {
                 _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
             }
+
+            _animator.SetBool(IsGroundKey, isGrounded);
+            _animator.SetFloat(VerticalVelocity, _rigidbody.velocity.y);
+            _animator.SetBool(IsRunning, _directionHorizontal != 0);
+
+            UpdateSpriteDerection();
+        }
+
+        private void UpdateSpriteDerection()
+        {
+            if (_directionHorizontal > 0)
+            {
+                _sprite.flipX = false;
+            }
+            else if (_directionHorizontal < 0)
+            {
+                _sprite.flipX = true;
+            }
         }
 
         private bool IsGrounded()
@@ -57,7 +84,7 @@ namespace PixelCrew
 
         public void SaySomething()
         {
-            Debug.Log("Я могу говорить!!!");
+            Debug.Log("ХУЙ ПИЗДА ДЖИГУРДА");
         }
     }
 
